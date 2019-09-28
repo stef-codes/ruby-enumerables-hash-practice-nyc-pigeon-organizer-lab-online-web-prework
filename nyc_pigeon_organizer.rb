9babc0e2fbcc2a84aa32f9cb0177ec1d99112122
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-28 19:35:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   i = 0 
-  data[:color][:purple] 
+  data['result']['data'] = data['result']['data'].map{|v|  Hash[v.to_a.reverse]}
   i+= 1 
   
   end 
