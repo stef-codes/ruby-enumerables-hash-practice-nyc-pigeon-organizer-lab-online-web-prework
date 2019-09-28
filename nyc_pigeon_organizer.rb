28467938ba36208e17321ab912af96303c351008
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-28 20:12:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,11 @@
+require "pry"
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
 
   data.each do |attribute_name, attributes|
     attributes.each do |attribute_value, pigeon_names|
       pigeon_names.each do |name|
+        binding.pry
         pigeon_list[name] ||= {}
         pigeon_list[name][attribute_name] ||= []
         pigeon_list[name][attribute_name].push(attribute_value.to_s)
